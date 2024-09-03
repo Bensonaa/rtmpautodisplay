1bef6da06c2a8701d47dcabe29d169b6fcf3f5a9
Bensonaa <aa.benson1@gmail.com>
2024-09-02 23:09:26 -0600

    Update stream.py

--- stream.py
+++ stream.py
@@ -9,14 +9,6 @@ def is_stream_active(url):
     except subprocess.TimeoutExpired:
         return False
 
-def is_display_connected(display):
-    try:
-        result = subprocess.run(['tvservice', '-s', display], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return 'HDMI' in result.stdout.decode()
-    except Exception as e:
-        print(f"Error checking display {display}: {e}")
-        return False
-
 def show_image(image_path, hdmi):
     subprocess.Popen(['feh', '-F', '--on-top', '--auto-zoom', '--fullscreen', '--display', hdmi, image_path])
 
@@ -26,31 +18,20 @@ def start_stream(url, image_path):
             print("Stream is active. Starting ffplay...")
             subprocess.run(['pkill', 'feh'])
             
-            processes = []
-            for display in ['1', '2']:
-                if is_display_connected(display):
-                    process = subprocess.Popen(['ffplay', '-fs', '-an', '-vcodec', 'h264_v4l2m2m', '-i', url])
-                    processes.append(process)
-                else:
-                    print(f"No display connected to HDMI{display}.")
-            
-            for process in processes:
-                process.wait()
+            process_hdmi1 = subprocess.Popen(['ffplay', '-fs', '-an', '-vcodec', 'h264_v4l2m2m', '-i', url])
+            process_hdmi1.wait()
             
             print("Stream disconnected. Showing image and restarting in 5 seconds...")
             show_image(image_path, 'hdmi')
-            show_image(image_path, 'hdmi2')
             time.sleep(5)
         else:
             print("Stream is not active. Showing image and checking again in 5 seconds...")
             show_image(image_path, 'hdmi')
-            show_image(image_path, 'hdmi2')
             time.sleep(5)
 
 if __name__ == "__main__":
     stream_url = "rtmp://10.0.0.62/bcs/channel0_ext.bcs?channel=0&stream=0&user=admin&password=curling1"
     image_path = "/home/pi/rpisurv/surveillance/images/connecting.png"
     show_image(image_path, 'hdmi')
-    show_image(image_path, 'hdmi2')
     time.sleep(5)
     start_stream(stream_url, image_path)
