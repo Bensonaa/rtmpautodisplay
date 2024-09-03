c3f3d41dfff57b786d9f480d83872d865f47944e
Bensonaa <aa.benson1@gmail.com>
2024-09-02 23:20:37 -0600

    Update stream.py

--- stream.py
+++ stream.py
@@ -9,8 +9,24 @@ def is_stream_active(url):
     except subprocess.TimeoutExpired:
         return False
 
-def show_image(image_path, hdmi):
-    subprocess.Popen(['feh', '-F', '--on-top', '--auto-zoom', '--fullscreen', '--display', hdmi, image_path])
+def get_connected_displays():
+    try:
+        result = subprocess.run(['xrandr'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = result.stdout.decode()
+        connected_displays = []
+        
+        for line in output.split('\n'):
+            if ' connected' in line:
+                display = line.split()[0]
+                connected_displays.append(display)
+        
+        return connected_displays
+    except Exception as e:
+        print(f"Error detecting connected displays: {e}")
+        return []
+
+def show_image(image_path, display):
+    subprocess.Popen(['feh', '-F', '--on-top', '--auto-zoom', '--fullscreen', '--display', display, image_path])
 
 def start_stream(url, image_path):
     while True:
@@ -18,20 +34,34 @@ def start_stream(url, image_path):
             print("Stream is active. Starting ffplay...")
             subprocess.run(['pkill', 'feh'])
             
-            process_hdmi1 = subprocess.Popen(['ffplay', '-fs', '-an', '-vcodec', 'h264_v4l2m2m', '-i', url])
-            process_hdmi1.wait()
+            connected_displays = get_connected_displays()
+            if connected_displays:
+                processes = []
+                for display in connected_displays:
+                    process = subprocess.Popen(['ffplay', '-fs', '-an', '-vcodec', 'h264_v4l2m2m', '-i', url])
+                    processes.append(process)
+                
+                for process in processes:
+                    process.wait()
+            else:
+                print("No active displays connected.")
             
             print("Stream disconnected. Showing image and restarting in 5 seconds...")
-            show_image(image_path, 'hdmi')
+            for display in connected_displays:
+                show_image(image_path, display)
             time.sleep(5)
         else:
             print("Stream is not active. Showing image and checking again in 5 seconds...")
-            show_image(image_path, 'hdmi')
+            connected_displays = get_connected_displays()
+            for display in connected_displays:
+                show_image(image_path, display)
             time.sleep(5)
 
 if __name__ == "__main__":
     stream_url = "rtmp://10.0.0.62/bcs/channel0_ext.bcs?channel=0&stream=0&user=admin&password=curling1"
     image_path = "/home/pi/rpisurv/surveillance/images/connecting.png"
-    show_image(image_path, 'hdmi')
+    connected_displays = get_connected_displays()
+    for display in connected_displays:
+        show_image(image_path, display)
     time.sleep(5)
     start_stream(stream_url, image_path)
