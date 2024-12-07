27540800787cf6294e5aa5e4370548de56401b01
Bensonaa <aa.benson1@gmail.com>
2024-12-07 12:09:47 -0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -5,10 +5,14 @@ import logging
 import psutil
 
 class StreamManager:
-    def __init__(self, url1, url2=None, image_path=None):
+    def __init__(self, url1, url2=None, image_path=None, youtube_url1=None, youtube_key1=None, youtube_url2=None, youtube_key2=None):
         self.url1 = url1
         self.url2 = url2
         self.image_path = image_path
+        self.youtube_url1 = youtube_url1
+        self.youtube_key1 = youtube_key1
+        self.youtube_url2 = youtube_url2
+        self.youtube_key2 = youtube_key2
         self.lock = threading.Lock()
         self.ffplay_processes = []
 
@@ -38,6 +42,21 @@ class StreamManager:
             ffplay_process.terminate()
             ffplay_process.wait()
 
+    def stream_to_youtube(self, input_url, youtube_url, youtube_key):
+        command = [
+            'ffmpeg', '-re', '-i', input_url, '-c:v', 'libx264', '-preset', 'veryfast', '-maxrate', '3000k', '-bufsize', '6000k', '-pix_fmt', 'yuv420p', '-g', '50', '-c:a', 'aac', '-b:a', '160k', '-ar', '44100', '-f', 'flv', f'{youtube_url}/{youtube_key}'
+        ]
+        with self.lock:
+            ffmpeg_process = subprocess.Popen(command)
+            self.ffplay_processes.append(ffmpeg_process)
+        try:
+            ffmpeg_process.communicate()
+        except Exception as e:
+            logging.error(f"Error streaming to YouTube: {e}")
+        finally:
+            ffmpeg_process.terminate()
+            ffmpeg_process.wait()
+
     def monitor_cpu_usage(self):
         while True:
             with self.lock:
@@ -65,10 +84,19 @@ class StreamManager:
                 play_thread1 = threading.Thread(target=self.play_stream, args=(self.url1, 0, 0, 1920, 1080))  # Left half of the screen
                 play_thread1.start()
 
+                if self.youtube_url1 and self.youtube_key1:
+                    youtube_thread1 = threading.Thread(target=self.stream_to_youtube, args=(self.url1, self.youtube_url1, self.youtube_key1))
+                    youtube_thread1.start()
+
                 if self.url2 and self.is_stream_active(self.url2):
                     logging.info("Stream 2 is active. Starting ffplay...")
                     play_thread2 = threading.Thread(target=self.play_stream, args=(self.url2, 1920, 0, 1920, 1080))  # Right half of the screen
                     play_thread2.start()
+
+                    if self.youtube_url2 and self.youtube_key2:
+                        youtube_thread2 = threading.Thread(target=self.stream_to_youtube, args=(self.url2, self.youtube_url2, self.youtube_key2))
+                        youtube_thread2.start()
+
                     play_thread2.join()
 
                 play_thread1.join()
@@ -91,7 +119,11 @@ if __name__ == "__main__":
     stream_url1 = "rtmp://192.168.1.74/bcs/channel0_ext.bcs?channel=0&stream=0&user=admin&password=curling1"
     stream_url2 = None
     image_path = "/home/pi/rtmpautodisplay/placeholder.png"
+    youtube_url1 = "rtmp://a.rtmp.youtube.com/live2"
+    youtube_key1 = "YOUR_YOUTUBE_STREAM_KEY_1"
+    youtube_url2 = None
+    youtube_key2 = None
     
     time.sleep(5)
-    stream_manager = StreamManager(stream_url1, stream_url2, image_path)
+    stream_manager = StreamManager(stream_url1, stream_url2, image_path, youtube_url1, youtube_key1, youtube_url2, youtube_key2)
     stream_manager.start_stream()
